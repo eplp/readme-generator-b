c1ab7b5ff04619ae2a8bec0339ff3ab1c040b60e
eplp <edgardopina57@gmail.com>
2023-12-14 17:18:49 -0700

    Update package.json to add type: module, added and validated inquirer first prompt

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-// TODO: Include packages needed for this application
+//* Include packages needed for this application
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+   {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'Enter the project title:',
+   },
+];
 
+inquirer.prompt(questions).then((answers) => {
+   console.log('answers:', answers);
+});
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
