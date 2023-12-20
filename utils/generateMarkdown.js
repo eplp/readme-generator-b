cc458f5e19dfd744f60b9793b4e986d80c58332f
eplp <edgardopina57@gmail.com>
2023-12-20 00:40:01 -0700

    Fix to profile username bug

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ export const generateMarkdown = async (answers, licenseKeysList, githubApi) => {
    //
    const { appLicense, licenseDescription } = await license(answers.licenseType, licenseKeysList, githubApi);
 
-   return `## ${answers.projectTitle}   ${`![](assets/images/badge.svg)`}
+   return `## ${answers.projectTitle}   ${'![](assets/images/badge.svg)'}
 ### Description
 ${answers.description}
 ${
@@ -19,11 +19,10 @@ ${
 ${answers.confirmUofU ? `- [U of U - Bootcamp](#u-of-u---bootcamp)` : ``}`
       : ``
 }
-### Installation  
+### Installation
 ${answers.installation}
 ### Usage
 ${answers.usage}
-![README.md Generator](assets/images/readme-generator.jpg)
 ### How to contribute
 ${answers.contribute}
 ### Tests
@@ -31,7 +30,7 @@ ${answers.tests}
 ### License
 This work is distributed under the license type: ${appLicense[0].key + ' / ' + answers.licenseType}. ${licenseDescription}
 ### Questions
-If you have any questions, visit my GitHub profile page: [github.com/${answers.userName}](github.com/${answers.userName}) or contact me at: [${answers.email}](mailto:${answers.email}).
+If you have any questions, visit my GitHub profile page: [github.com/${answers.userName}](https://github.com/${answers.userName}) or contact me at: [${answers.email}](mailto:${answers.email}).
 ${
    answers.confirmUofU
       ? `### U of U - Bootcamp
